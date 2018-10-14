596f569003f6ad4168df07c645c00d09ae1b15f8
Henri Blancke <henri@addapp.io>
2018-10-13 21:00:28 -0400

    [UPD] Upgrade to icebergh-mongo-models

--- index.js
+++ index.js
@@ -1,62 +1,65 @@
 'use strict';
 const Hoek = require('hoek');
-const MongoModels = require('mongo-models');
+const MongoModels = require('icebergh-mongo-models');
 
 
 const register = async function (server, options) {
 
-    Hoek.assert(options.models, 'options.models is required');
-    Hoek.assert(options.mongodb, 'options.mongodb is required');
-    Hoek.assert(options.mongodb.connection, 'options.mongodb.connection is required');
-    Hoek.assert(options.mongodb.connection.uri, 'options.mongodb.connection.uri is required');
-    Hoek.assert(options.mongodb.connection.db, 'options.mongodb.connection.db is required');
+  Hoek.assert(options.models, 'options.models is required');
+  Hoek.assert(options.mongodb, 'options.mongodb is required');
+  Hoek.assert(options.mongodb.connection, 'options.mongodb.connection is required');
+  Hoek.assert(options.mongodb.connection.uri, 'options.mongodb.connection.uri is required');
+  Hoek.assert(options.mongodb.connection.db, 'options.mongodb.connection.db is required');
 
-    const modelModules = options.models.reduce((accumulator, path) => {
+  const modelModules = options.models.reduce((accumulator, path) => {
 
-        accumulator[path] = require(path);
+    accumulator[path] = require(path);
 
-        return accumulator;
-    }, {});
+    return accumulator;
+  }, {});
 
-    server.expose('mongo-models', MongoModels);
+  server.expose('mongo-models', MongoModels);
 
-    server.ext({
-        type: 'onPreStart',
-        method: async function (_server) {
+  server.ext({
+    type: 'onPreStart',
+    method: async function (_server) {
 
-            if (options.hasOwnProperty('autoIndex') && options.autoIndex === false) {
-                return;
-            }
+      if (options.hasOwnProperty('autoIndex') && options.autoIndex === false) {
+        return;
+      }
 
-            const indexJobs = options.models
-                .map((path) => modelModules[path])
-                .filter((model) => Boolean(model.indexes))
-                .map((model) => model.createIndexes.bind(model, model.indexes));
+      const indexJobs = options.models
+        .map((path) => modelModules[path])
+        .filter((model) => Boolean(model.indexes))
+        .map((model) => {
+          console.log(`Creating indexes for ${model.collectionName}, ${model.indexes}`);
+          model.createIndexes.bind(model, model.indexes);
+        });
 
-            await Promise.all(indexJobs);
+      await Promise.all(indexJobs);
 
-            server.log(['info', 'mongodb'], 'HapiMongoModels: finished processing auto indexes.');
-        }
-    });
+      server.log(['info', 'mongodb'], 'HapiMongoModels: finished processing auto indexes.');
+    }
+  });
 
-    server.ext({
-        type: 'onPostStop',
-        method: function (_server) {
+  server.ext({
+    type: 'onPostStop',
+    method: function (_server) {
 
-            MongoModels.disconnect();
+      MongoModels.disconnect();
 
-            server.log(['info', 'mongodb'], 'HapiMongoModels: closed db connection(s).');
-        }
-    });
+      server.log(['info', 'mongodb'], 'HapiMongoModels: closed db connection(s).');
+    }
+  });
 
-    await MongoModels.connect(options.mongodb.connection, options.mongodb.options);
+  await MongoModels.connect(options.mongodb.connection, options.mongodb.options);
 
-    server.log(['info', 'mongodb'], 'HapiMongoModels: successfully connected to the db.');
+  server.log(['info', 'mongodb'], 'HapiMongoModels: successfully connected to the db.');
 };
 
 
 module.exports = {
-    name: 'hapi-mongo-models',
-    pkg: require('./package.json'),
-    register
+  name: 'hapi-mongo-models',
+  pkg: require('./package.json'),
+  register
 };
